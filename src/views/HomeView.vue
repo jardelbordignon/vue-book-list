c11a2272c202fa9e7a4e1b6f990f94f96ae51b36
Jardel Bordignon <jardel1101@gmail.com>
2024-08-22 13:23:14 -0300

    :sparkles: reactivity with ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+// ref para tipos primitivos como number, string, boolean
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
 
 <template>
   <div>
     <h2>Hello Composition API</h2>
+    <h1>Count: {{ count }}</h1>
+    <!-- <button @click="count++">Increment</button> -->
+    <button @click="increment">Increment</button>
   </div>
 </template>
