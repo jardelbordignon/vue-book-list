8d755dafbec75a56ba6bf5001d4ec63e71672464
Jardel Bordignon <jardel1101@gmail.com>
2024-08-22 13:39:33 -0300

    :sparkles: reactivity with reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,37 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
 // ref para tipos primitivos como number, string, boolean
-const count = ref(0)
+//const count = ref(0)
 
-function increment() {
-  count.value++
+// reactive para objetos
+const courses = reactive([{ title: 'Vue 3' }, { title: 'React' }, { title: 'Node' }])
+
+let newCourse = {}
+
+// function increment() {
+//   count.value++
+// }
+
+function addCourse() {
+  courses.push(newCourse)
+  newCourse = {}
 }
 </script>
 
 <template>
   <div>
-    <h2>Hello Composition API</h2>
-    <h1>Count: {{ count }}</h1>
-    <!-- <button @click="count++">Increment</button> -->
-    <button @click="increment">Increment</button>
+    <h1>Courses List</h1>
+
+    <ul>
+      <li v-for="course in courses" :key="course.title">
+        <p>{{ course.title }}</p>
+      </li>
+    </ul>
+
+    <br />
+
+    <input type="text" v-model="newCourse.title" />
+    <button type="button" @click="addCourse">Adicionar</button>
   </div>
 </template>
