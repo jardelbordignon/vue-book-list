f2af18fed392ccb838a5e2b986223ab6b3d36f21
Jardel Bordignon <jardel1101@gmail.com>
2024-08-22 13:14:26 -0300

    :broom: app cleaning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2>Hello Composition API</h2>
+  </div>
 </template>
