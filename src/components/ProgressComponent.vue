9b95bc232fd92f22c37373ebf71971ff306ba056
Jardel Bordignon <jardel1101@gmail.com>
2024-08-22 14:38:02 -0300

    :sparkles: computed props

--- src/components/ProgressComponent.vue
+++ src/components/ProgressComponent.vue
@@ -1,12 +1,16 @@
 <script setup>
-defineProps({
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
   courses: Array
 })
+
+const coursesDone = computed(() => props.courses.filter(({ done }) => done).length)
 </script>
 
 <template>
   <div>
-    <p>Courses {{ courses.length }}</p>
+    <p>{{ `${coursesDone} cursos completos de um total de ${courses.length}` }}</p>
   </div>
 </template>
 
